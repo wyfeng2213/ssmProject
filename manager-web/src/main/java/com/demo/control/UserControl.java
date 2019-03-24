cdcf6bb85ee5d9df5d54eb3fe2cb04c24c77ea32
wyfeng2213 <874999821@qq.com>
2019-03-24 21:46:38 +0800

    添加redis集群

--- manager-web/src/main/java/com/demo/control/UserControl.java
+++ manager-web/src/main/java/com/demo/control/UserControl.java
@@ -1,18 +1,23 @@
 package com.demo.control;
 
+import com.alibaba.fastjson.JSON;
 import com.demo.service.IUserService;
 import com.demo.util.BaseResult;
+import com.demo.util.CookieUtils;
 import com.demo.util.EgoResult;
 import com.demo.util.TbUser;
 import com.example.model.Users;
+import com.fasterxml.jackson.annotation.JsonAlias;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import redis.clients.jedis.JedisCluster;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/user")
@@ -46,11 +51,19 @@ public class UserControl {
         return "register";
     }
 
+    @Resource
+    private JedisCluster jedisClients;
     @RequestMapping("/login")
     @ResponseBody
     public EgoResult login(TbUser user, HttpServletRequest request, HttpServletResponse response){
         EgoResult r=new EgoResult();
         if(user.getUsername().equals("zs")&&user.getPassword().equals("123456")){
+            String key = UUID.randomUUID().toString();
+            jedisClients.set(key, JSON.toJSONString(user));
+            jedisClients.expire(key,60*60*24*7);
+            //产生 Cookie
+            CookieUtils.setCookie(request, response,
+                    "TT_TOKEN", key, 60*60*24*7);
             r.setMsg("成功");
             r.setData("成功");
             r.setStatus(200);
