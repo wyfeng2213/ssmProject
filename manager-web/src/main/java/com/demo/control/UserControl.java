7fb560f6e981d385640b988ff9cd5fd36ae30f8a
wyfeng2213 <874999821@qq.com>
2019-03-24 20:58:16 +0800

    模拟登陆流程

--- manager-web/src/main/java/com/demo/control/UserControl.java
+++ manager-web/src/main/java/com/demo/control/UserControl.java
@@ -1,12 +1,17 @@
 package com.demo.control;
 
 import com.demo.service.IUserService;
+import com.demo.util.BaseResult;
+import com.demo.util.EgoResult;
+import com.demo.util.TbUser;
 import com.example.model.Users;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
@@ -29,5 +34,31 @@ public class UserControl {
     public List<Users> findUser(){
         List<Users> list = userServiceImpl.findUser();
         return list;
+    }
+
+    @RequestMapping("/showLogin")
+    public String Login(){
+        return "login";
+    }
+
+    @RequestMapping("/showRegister")
+    public String showRegister(){
+        return "register";
+    }
+
+    @RequestMapping("/login")
+    @ResponseBody
+    public EgoResult login(TbUser user, HttpServletRequest request, HttpServletResponse response){
+        EgoResult r=new EgoResult();
+        if(user.getUsername().equals("zs")&&user.getPassword().equals("123456")){
+            r.setMsg("成功");
+            r.setData("成功");
+            r.setStatus(200);
+        }  else {
+            r.setMsg("失败");
+            r.setData("失败");
+            r.setStatus(201);
+        }
+        return r;
     }
 }
